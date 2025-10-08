d35ba5ca187c4f0f54ef246e00580156385c6988
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-10-08 13:40:11 +0200

    vrai

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,9 +3,9 @@ import type { TypedPocketBase } from "./pocketbase-types";
 
 var path = '';
 if (import.meta.env.MODE === 'development')
-    path = 'http://localhost:8095';
+    path = 'http://localhost:8090';
 else
-    path = 'http://localhost:8095'; // port local de ton PB sur le VPS
+    path = 'http://localhost:8085'; // port local de ton PB sur le VPS
 
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
